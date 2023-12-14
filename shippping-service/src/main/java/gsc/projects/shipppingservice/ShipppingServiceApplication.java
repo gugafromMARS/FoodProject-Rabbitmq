aa9f4744c505b8e0ba73c12af5b61cc65acafc50
Gonçalo <goncaloscravo@gmail.com>
2023-12-14 00:29:47 +0000

    Initial commit

---
+++ shippping-service/src/main/java/gsc/projects/shipppingservice/ShipppingServiceApplication.java
@@ -0,0 +1,13 @@
+package gsc.projects.shipppingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipppingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipppingServiceApplication.class, args);
+	}
+
+}
