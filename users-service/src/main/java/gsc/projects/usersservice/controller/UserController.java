8a080331b84a696a723b4c2748382f02745f5824
Gonçalo <goncaloscravo@gmail.com>
2023-12-14 19:43:32 +0000

    Add: user endpoints implementation

--- users-service/src/main/java/gsc/projects/usersservice/controller/UserController.java
+++ users-service/src/main/java/gsc/projects/usersservice/controller/UserController.java
@@ -1,16 +1,39 @@
 package gsc.projects.usersservice.controller;
 
 
+import gsc.projects.usersservice.dto.UserCreateDto;
+import gsc.projects.usersservice.dto.UserUpdateDto;
 import gsc.projects.usersservice.service.UserServiceImp;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
 
-
     private final UserServiceImp userServiceImp;
+
+    @PostMapping
+    public ResponseEntity<?> create(UserCreateDto userCreateDto){
+        return new ResponseEntity<>(userServiceImp.createUser(userCreateDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userEmail}")
+    public ResponseEntity<?> getUser(@PathVariable("userEmail") String userEmail){
+        return ResponseEntity.ok(userServiceImp.getUserByEmail(userEmail));
+    }
+
+    @DeleteMapping("/{userEmail}")
+    public ResponseEntity<?> delete(@PathVariable ("userEmail") String userEmail){
+        userServiceImp.deleteUserByEmail(userEmail);
+        return new ResponseEntity<>("User deleted successfully!", HttpStatus.OK);
+    }
+
+    @PutMapping("/{userEmail}")
+    public ResponseEntity<?> update(@PathVariable ("userEmail") String userEmail, UserUpdateDto userUpdateDto){
+        return ResponseEntity.ok(userServiceImp.updateByEmail(userEmail, userUpdateDto));
+    }
 }
--- users-service/src/main/java/gsc/projects/usersservice/service/UserServiceImp.java
+++ users-service/src/main/java/gsc/projects/usersservice/service/UserServiceImp.java
@@ -2,9 +2,15 @@ package gsc.projects.usersservice.service;
 
 
 import gsc.projects.usersservice.converter.UserConverter;
+import gsc.projects.usersservice.dto.UserCreateDto;
+import gsc.projects.usersservice.dto.UserDto;
+import gsc.projects.usersservice.dto.UserUpdateDto;
+import gsc.projects.usersservice.model.User;
 import gsc.projects.usersservice.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @AllArgsConstructor
@@ -14,4 +20,51 @@ public class UserServiceImp {
     private final UserConverter userConverter;
 
 
+    public UserDto createUser(UserCreateDto userCreateDto) {
+        User existingUser = userRepository.findByEmail(userCreateDto.getEmail());
+        if(existingUser != null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User email already exists");
+        }
+        existingUser = userConverter.fromCreateDto(userCreateDto);
+        userRepository.save(existingUser);
+        return userConverter.toDto(existingUser);
+    }
+
+
+    public UserDto getUserByEmail(String userEmail) {
+        User existingUser = userRepository.findByEmail(userEmail);
+        if(existingUser == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User email not found");
+        }
+        return userConverter.toDto(existingUser);
+    }
+
+
+    public void deleteUserByEmail(String userEmail) {
+        User existingUser = userRepository.findByEmail(userEmail);
+        if(existingUser == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User email not found");
+        }
+        userRepository.delete(existingUser);
+    }
+
+    public UserDto updateByEmail(String userEmail, UserUpdateDto userUpdateDto) {
+        User existingUser = userRepository.findByEmail(userEmail);
+        if(existingUser == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User email not found");
+        }
+        if(userUpdateDto.getEmail() != null && userUpdateDto.getAddress() != null){
+            existingUser.setEmail(userUpdateDto.getEmail());
+            existingUser.setAddress(userUpdateDto.getAddress());
+            return userConverter.toDto(existingUser);
+        }
+        if(userUpdateDto.getEmail() != null && userUpdateDto.getAddress() == null){
+            existingUser.setEmail(userUpdateDto.getEmail());
+            return userConverter.toDto(existingUser);
+        }
+        if(userUpdateDto.getAddress() != null && userUpdateDto.getEmail() == null){
+            existingUser.setAddress(userUpdateDto.getAddress());
+        }
+        return userConverter.toDto(existingUser);
+    }
 }
