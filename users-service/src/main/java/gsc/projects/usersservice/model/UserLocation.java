9f3566e4cb6135584c9d625d655a39c8f3e21f97
Gonçalo <goncaloscravo@gmail.com>
2023-12-15 18:45:58 +0000

    Add: nearby search api implementation on user service

---
+++ users-service/src/main/java/gsc/projects/usersservice/model/UserLocation.java
@@ -0,0 +1,13 @@
+package gsc.projects.usersservice.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLocation {
+
+    private String latitude;
+    private String longitude;
+}
